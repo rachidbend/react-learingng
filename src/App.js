4204e6e63e5f9657747d898b6198541215ec9e6d
rachidbend <rachidbendaghor@gmail.com>
2022-08-23 19:08:12 +0100

    frst commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
+const friends = ['Rachid', 'Taha', 'Hicham', 'Soad', 'Rida'];
+
+const changeColor = function (e) {
+  e.target.classList.toggle('redling');
+};
+
+const list = (
+  <div>
+    <h1 className="heading heading--primary">A list of my friends</h1>
+    <ul>
+      {friends.map(friend => {
+        return (
+          <li onClick={changeColor}>
+            <a href="#" id={'friend__' + friend.toLowerCase()}>
+              {' '}
+              {friend}{' '}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  </div>
+);
+
+const form = (
+  <form className="form">
+    <label for="number">write a number</label>
+    <input type="number" id="number"></input>
+    <button type="submit">submit</button>
+  </form>
+);
+
+const container = (
+  <div className="container">
+    {list}
+    <p>devider</p>
+    {form}
+  </div>
+);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return container;
 }
 
 export default App;
